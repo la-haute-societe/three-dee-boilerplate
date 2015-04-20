49c73f8c988d804d653515f5a20c51ae8bacae3b
zouloux <alexis@lahautesociete.com>
2015-04-20 09:23:47 +0200

    First commit Grunt Config Bower Config Typescript Definitions Bower libs

---
+++ build/Gruntfile.js
@@ -0,0 +1,182 @@
+// ----------------------------------------------------------------------------- FILES
+
+var config = {
+
+    // Path to the main typescript app file
+    typescriptAppFile: 'src/app/App',
+
+    input: {
+
+        libs: [
+            // Include TweenLite
+            '../lib/gsap/src/minified/TweenLite.min.js',
+            '../lib/gsap/src/minified/TimelineLite.min.js',
+            '../lib/gsap/src/minified/easing/*.js',
+            '../lib/gsap/src/minified/plugins/*.js',
+
+            // Include require
+            '../lib/requirejs/require.js',
+
+            // Include three
+            '../lib/three.js/three.min.js'
+        ],
+
+        // Compiled typescript files (not included if not imported in App code)
+        typescript: [
+            '../lib/**/*.ts',
+            '../src/**/*.ts'
+        ]
+    },
+
+    output: {
+        // Single javascript output big boy file
+        javascript: '../deploy/script.js',
+
+        // Temporary typescript folder (before optimisation)
+        // and output file (optimised)
+        typescript: {
+            folder: 'temp/typescript/',
+            file: 'temp/typescript.js'
+        }
+    }
+};
+
+// ----------------------------------------------------------------------------- PLUGINS
+module.exports = function (grunt)
+{
+    // Load grunt plugins
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-clean-ts-extends');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+    // Project and plugins configuration
+    grunt.initConfig({
+
+        // --------------------------------------------------------------------- UTILS
+
+        // Clean folders / files before transpiling
+        clean: {
+            typescript: [
+                config.output.typescript.folder
+            ]
+        },
+
+        // --------------------------------------------------------------------- SCRIPTS
+
+        // Configure typescript to javascript compilation
+        typescript: {
+            all: {
+                options: {
+                    module: 'amd',
+                    target: 'es5',
+                    basePath: '../',
+
+                    sourceMap: false,
+                    declaration: false,
+                    noResolve: false,
+
+                    references: [
+                        // TSD definitions
+                        "typings/**/*.d.ts"
+                    ]
+                },
+                src: config.input.typescript,
+                dest: config.output.typescript.folder
+            }
+        },
+
+        // Optimize commonjs modules from typescript
+        requirejs: {
+            all: {
+                options: {
+                    baseUrl: config.output.typescript.folder,
+
+                    // Name of the app file
+                    name: config.typescriptAppFile,
+
+                    // Output typescript file
+                    out: config.output.typescript.file,
+
+                    // Other stuff
+                    paths: {},
+                    findNestedDependencies: false,
+                    wrap: true,
+                    optimize: "none"
+                }
+            }
+        },
+
+        // Clean extends statements added by typescript compiler
+        cleanTsExtends: {
+            src: config.output.typescript.file
+        },
+
+        // Concat all javascript files in one
+        concat: {
+            options: {
+                separator: ';\n',
+                stripBanners: true
+                //banner: '"use strict";\n'
+            },
+
+            js: {
+                src: [config.input.libs].concat([
+                    config.output.typescript.file
+                ]),
+                dest: config.output.javascript
+            }
+        },
+
+        // Obfuscate, compress and disable logs on concatenated javascript file
+        uglify: {
+            options: {
+                mangle: false,
+                footer: ""
+            },
+            js: {
+                src: config.output.javascript,
+                dest: config.output.javascript
+            }
+        },
+
+        // --------------------------------------------------------------------- WATCH
+
+        // Configure watch to auto-recompile when a file is updated
+        watch: {
+            options: {
+                livereload  : true,
+                interrupt   : false
+            },
+
+            // Watch script sources files
+            script: {
+                files: config.input.typescript,
+                tasks: ['script']
+            },
+
+            // Watch changes on this file
+            grunt: {
+                files: ['Gruntfile.js'],
+                tasks: ['dev']
+            }
+        }
+    });
+
+    // ------------------------------------------------------------------------- TASKS
+
+    // Get all scripts in one JS file
+    grunt.registerTask('script', ['clean:typescript', 'typescript:all', 'requirejs:all', 'cleanTsExtends', 'concat:js']);
+
+    // Local dev task (without code compression / obfuscation)
+    grunt.registerTask('dev', ['script']);
+
+    // Production task (with code compression / obfuscation)
+    grunt.registerTask('dist', ['dev', 'uglify:js']);
+
+    // Run "dev" and watch on default
+    grunt.registerTask('default', ['dev', 'watch']);
+};
---
+++ src/three/ThreeEngine.ts
@@ -0,0 +1,153 @@
+export = ThreeEngine;
+
+module ThreeEngine
+{
+	export interface IEngineConfig
+	{
+		antialias				?:boolean;
+		backgroundColor			?:number;
+		backgroundAlpha			?:number;
+		autoPixelRatio			?:boolean;
+
+		cameraFov				?:number;
+		cameraNear				?:number;
+		cameraFar				?:number;
+	}
+
+	export var DEFAULT_ENGINE_CONFIG:IEngineConfig = {
+		antialias		: true,
+		backgroundColor	: 0xFFFFFF,
+		backgroundAlpha	: 1,
+		autoPixelRatio	: true,
+
+		cameraFov		: 70,
+		cameraNear		: 10,
+		cameraFar		: 10000
+	};
+
+	export class SceneBase
+	{
+		private _renderer				:THREE.Renderer;
+		get renderer ():THREE.Renderer { return this._renderer; }
+
+
+		private _camera					:THREE.PerspectiveCamera;
+		get camera ():THREE.PerspectiveCamera { return this._camera; }
+
+
+		private _scene					:THREE.Scene;
+		get scene ():THREE.Scene { return this._scene; }
+
+		private _renderSize				:number[];
+		get renderSize ():number[] { return this._renderSize; }
+
+
+
+		/**
+		 * Init Three.js engine
+		 */
+		initEngine (pConfig:IEngineConfig = DEFAULT_ENGINE_CONFIG):boolean
+		{
+			// Check if our system support WebGL rendering
+			if (this.isWebglAvailable())
+			{
+				// Instantiate the WebGL renderer
+				this._renderer = new THREE.WebGLRenderer({
+					antialias: pConfig.antialias
+				});
+
+				// Set the background color
+				(<THREE.WebGLRenderer> this._renderer).setClearColor(pConfig.backgroundColor, pConfig.backgroundAlpha);
+			}
+			else return false;
+
+			// Define pixel ratio
+			if (pConfig.autoPixelRatio)
+			{
+				this._renderer['setPixelRatio'](window.devicePixelRatio);
+			}
+
+			// Add the three dom element (canvas) to the stage dom element
+			document.getElementById("stage").appendChild(this._renderer.domElement);
+
+			// Init default perspective camera
+			this._camera = new THREE.PerspectiveCamera(pConfig.cameraFov, 1, pConfig.cameraNear, pConfig.cameraFar);
+
+			// Init scene
+			this._scene = new THREE.Scene();
+
+			// Listen for brower resize
+			window.onresize = this.windowResizedHandler;
+
+			// Update size once
+			this.windowResizedHandler();
+
+			// Init scene
+			this.initScene();
+
+			// Enable render loop
+			TweenLite.ticker.addEventListener("tick", () =>
+			{
+				this.enterFrameHandler();
+			});
+
+			// Render the first frame
+			this.enterFrameHandler();
+
+			// All is ok
+			return true;
+		}
+
+		/**
+		 * Called to init scene after engine. Have to be overrided via strategy pattern.
+		 */
+		initScene ():void { }
+
+		/**
+		 * Called every frames to run animations
+		 */
+		enterFrameHandler ():void
+		{
+			// Render a frame
+			this.renderer.render(this.scene, this.camera);
+		}
+
+		/**
+		 * Called when browser is resized
+		 */
+		windowResizedHandler = (pEvent:UIEvent = null):any =>
+		{
+			// Compute render size from stage dom element
+			this._renderSize = [window.innerWidth, window.innerHeight];
+
+			// Update camera aspect ratio
+			this._camera.aspect = this._renderSize[0] / this._renderSize[1];
+
+			// Update camera projection
+			this._camera.updateProjectionMatrix();
+
+			// Update renderer
+			this._renderer.setSize(this._renderSize[0], this._renderSize[1]);
+		};
+
+		/**
+		 * Detect WebGL capability
+		 */
+		private isWebglAvailable ()
+		{
+			try
+			{
+				var canvas = document.createElement("canvas");
+				return !!(
+					window["WebGLRenderingContext"] &&
+					(canvas.getContext("webgl") ||
+					canvas.getContext("experimental-webgl"))
+				);
+			}
+			catch (e)
+			{
+				return false;
+			}
+		}
+	}
+}
